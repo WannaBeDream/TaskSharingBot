eb214e7fdc4da6095ec7a9eb229c05b059a04b68
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-11-20 22:22:49 +0200

    Add new transitions

--- src/router/state-machine.js
+++ src/router/state-machine.js
@@ -1,11 +1,20 @@
 const appStates = require('./app-states');
 const userHomeCommands = require('../features/user-home/commands');
+const userAdCommands = require('../features/ad-setting/commands');
 const userHomeHandlers = require('../features/user-home/handlers');
 const settingsCommands = require('../features/user-settings/commands');
 const settingsHandlers = require('../features/user-settings/handlers');
 const generalCommands = require('./general-commands');
+const settingsAdHandlers = require('../features/ad-setting/handlers');
 
 const globalTransitions = {
+    [userHomeCommands.CREATE_AD.id]: { targetState: appStates.CREATE_AD },
+    [userHomeCommands.ALL_ADS.id]: { targetState: appStates.ALL_ADS },
+    [userHomeCommands.MY_ADS.id]: { targetState: appStates.MY_ADS },
+    [userHomeCommands.USER_SAVED_ADS.id]: { targetState: appStates.USER_SAVED_ADS },
+    [userAdCommands.PUBLISH_AD.id]: { targetState: appStates.PUBLISH_AD },
+    [userAdCommands.CANCEL_AD.id]: { targetState: appStates.CANCEL_AD },
+
     [userHomeCommands.USER_SETTINGS.id]: { targetState: appStates.USER_SETTINGS },
     [settingsCommands.CHANGE_LANG.id]: { targetState: appStates.CHANGE_LANGUAGE },
     [settingsCommands.VIEW_PROFILE.id]: { targetState: appStates.VIEW_PROFILE },
@@ -18,6 +27,10 @@ const globalTransitions = {
 };
 const settingsTransitions = {
     ...globalTransitions,
+    [generalCommands.GO_BACK.id]: { targetState: appStates.USER_SETTINGS }
+};
+
+const settingsAdTransitions = {
     [generalCommands.GO_BACK.id]: { targetState: appStates.USER_SETTINGS }
 };
 
@@ -72,5 +85,74 @@ module.exports = {
             handler: settingsHandlers.setLanguage,
             targetState: appStates.USER_SETTINGS
         }
+    },
+
+    [appStates.CREATE_AD.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.setTitle,
+            targetState: appStates.SET_DESCRIPTION
+        }
+    },
+
+    [appStates.SET_DESCRIPTION.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.setDescription,
+            targetState: appStates.SET_CATEGORY
+        }
+    },
+
+    [appStates.SET_CATEGORY.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.setCategory,
+            targetState: appStates.SET_RENUMERATION
+        }
+    },
+
+    [appStates.SET_RENUMERATION.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.setRenumeration,
+            targetState: appStates.SET_LOCATION
+        }
+    },
+
+    [appStates.SET_LOCATION.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.setLocation,
+            targetState: appStates.PREVIEW_AD
+        }
+    },
+
+    [appStates.PREVIEW_AD.id]: {
+        ...settingsAdTransitions,
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.publish,
+            targetState: appStates.PUBLISH_AD
+        },
+        [generalCommands.DATA_INPUT.id]: {
+            handler: settingsAdHandlers.publish,
+            targetState: appStates.PUBLISH_AD
+        }
+    },
+
+    [appStates.PUBLISH_AD.id]: {
+        ...globalTransitions,
+        [generalCommands.GO_BACK.id]: { targetState: appStates.USER_HOME }
+    },
+
+    [appStates.ALL_ADS.id]: {
+        ...globalTransitions
+    },
+
+    [appStates.MY_ADS.id]: {
+        ...globalTransitions
+    },
+
+    [appStates.USER_SAVED_ADS.id]: {
+        ...globalTransitions
     }
 };
