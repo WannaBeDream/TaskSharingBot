32ddedfd60375fc1cbdff3f81e1a991e38b69435
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-11-20 22:10:04 +0200

    Modify home view

--- src/features/user-home/commands.js
+++ src/features/user-home/commands.js
@@ -5,3 +5,35 @@ exports.USER_SETTINGS = {
         ua: 'Налаштування профілю'
     }
 };
+
+exports.USER_SAVED_ADS = {
+    id: 'userSavedAds',
+    title: {
+        en: 'Your saved ads',
+        ua: 'Збережені оголошення'
+    }
+};
+
+exports.ALL_ADS = {
+    id: 'allAds',
+    title: {
+        en: 'All ads',
+        ua: 'Усі оголошення'
+    }
+};
+
+exports.MY_ADS = {
+    id: 'myAds',
+    title: {
+        en: 'My ads',
+        ua: 'Мої оголошення'
+    }
+};
+
+exports.CREATE_AD = {
+    id: 'createAd',
+    title: {
+        en: 'Create ad',
+        ua: 'Створити оголошення'
+    }
+};
--- src/features/user-home/handlers.js
+++ src/features/user-home/handlers.js
@@ -1,19 +1,30 @@
+/* eslint-disable no-param-reassign */
 const { telegramTemplate } = require('claudia-bot-builder');
 const labels = require('./labels');
 const commands = require('./commands');
 
 exports.initUserHomeView = (update) => {
+    update.userState.act = 'USER_ACT';
     return new telegramTemplate.Text('\uD83D\uDE01')
-        .addReplyKeyboard([[commands.USER_SETTINGS.title[update.userState.lang]]], true)
+        .addReplyKeyboard(
+            [
+                [commands.CREATE_AD.title[update.userState.lang], commands.ALL_ADS.title[update.userState.lang]],
+                [commands.MY_ADS.title[update.userState.lang], commands.USER_SAVED_ADS.title[update.userState.lang]],
+                [commands.USER_SETTINGS.title[update.userState.lang]]
+            ],
+            true
+        )
         .get();
 };
 
 exports.getUserGreeting = (update) => {
+    update.userState.act = 'USER_ACT';
     const name = `${update.originalRequest.message.from.first_name} ${update.originalRequest.message.from.last_name}`;
     return new telegramTemplate.Text(labels.existingUserGreeting[update.userState.lang](name)).get();
 };
 
 exports.getNewUserGreeting = (update) => {
+    update.userState.act = 'USER_ACT';
     const name = `${update.originalRequest.message.from.first_name} ${update.originalRequest.message.from.last_name}`;
     return new telegramTemplate.Text(labels.newUserGreeting[update.userState.lang](name)).get();
 };
