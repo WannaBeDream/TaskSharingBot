c19deae67696fd3fbf1a9a94d7e81634cfac64bd
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-11-20 22:38:52 +0200

    Add logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const { MONGO_URI } = require('../config');
 const appStateDao = require('./app-state-mongo');
 const commandParser = require('./commandParser');
@@ -6,28 +7,47 @@ const langResources = require('./labels');
 const { connect } = require('../database/create-connection');
 
 module.exports = async (update) => {
-    // connect to mongo
-    await connect(MONGO_URI); // create try catch
-
-    // get Id
-    const userId = update.originalRequest.message.from.id;
-
-    // get user`s state
-    const userState = await appStateDao.getUserState(userId);
-
-    // parse input command
-    const command = commandParser(update.text, userState.lang);
-
-    //
-    const transition = STATE_MACHINE[userState.appStateId][command.id];
-
-    if (!transition) {
-        return langResources.unknownCommand[userState.lang];
-    }
-    // eslint-disable-next-line no-param-reassign
-    update.userState = userState;
     try {
-        const reply = [];
+        await connect(MONGO_URI); // TODO REDO https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/#example
+
+        // Handle data from inline keyboard
+        if (update.originalRequest.callback_query !== undefined) {
+            const { idAd, op } = JSON.parse(update.originalRequest.callback_query.data);
+
+            // Todo - make constants
+            switch (op) {
+                case 'ADD_TO_SAVED':
+                    await appStateDao.addToSaved(update.originalRequest.callback_query.from.id, idAd);
+                    break;
+                case 'DELETE_FROM_SAVED':
+                    await appStateDao.deleteFromSaved(update.originalRequest.callback_query.from.id, idAd);
+                    break;
+                case 'DELETE_MY_AD':
+                    await appStateDao.deleteMyAd(idAd);
+                    break;
+                default:
+                    break;
+            }
+            // Todo - land, message
+            return 'Done';
+        }
+
+        const userId = update.originalRequest.message.from.id;
+
+        const userState = await appStateDao.getUserState(userId);
+        const advertisementState = await appStateDao.getAdState(userId);
+
+        const command = commandParser(update.text, userState.lang);
+
+        const transition = STATE_MACHINE[userState.appStateId][command.id];
+
+        if (!transition) {
+            return langResources.unknownCommand[userState.lang];
+        }
+        update.userState = userState;
+        update.advertisementState = advertisementState;
+
+        let reply = [];
         if (transition.handler) {
             const commandProcessResult = transition.handler(update);
             if (commandProcessResult) {
@@ -40,9 +60,52 @@ module.exports = async (update) => {
         } else {
             reply.push(markup);
         }
-        await appStateDao.setUserState(userId, { ...userState, appStateId: transition.targetState.id });
+
+        const action = { ...userState };
+
+        let ads = [];
+        // переменная устанавливается во время действий
+        switch (action.act) {
+            case 'USER_ACT':
+                await appStateDao.updateUser(userId, { ...userState, appStateId: transition.targetState.id });
+                break;
+            case 'AD_ACT':
+                await appStateDao.updateUser(userId, { ...userState, appStateId: transition.targetState.id });
+                await appStateDao.updateAdvertisement(userId, {
+                    ...userState,
+                    appStateId: transition.targetState.id
+                });
+                break;
+            case 'AD_DISPLAY_ALL_ACT':
+                await appStateDao.updateUser(userId, { ...userState, appStateId: transition.targetState.id });
+                ads = await appStateDao.findAllAds(userId);
+                break;
+            case 'AD_DISPLAY_MY_ACT':
+                await appStateDao.updateUser(userId, { ...userState, appStateId: transition.targetState.id });
+                ads = await appStateDao.findMyAdss(userId);
+                break;
+            case 'AD_DISPLAY_SAVED_ACT':
+                await appStateDao.updateUser(userId, { ...userState, appStateId: transition.targetState.id });
+                ads = await appStateDao.findSavedAdss(userId);
+                break;
+            default:
+                break;
+        }
+        // If ads were found
+        if (ads.length !== 0) {
+            update.advertisements = ads;
+            reply = [];
+            const markup2 = transition.targetState.constructor(update);
+            if (Array.isArray(markup2)) {
+                reply.push(...markup2);
+            } else {
+                reply.push(markup2);
+            }
+            return reply;
+        }
         return reply;
     } catch (error) {
+        // Todo
         return error.message;
     }
 };
