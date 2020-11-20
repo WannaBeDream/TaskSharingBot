a2c090fe8d2486bdbbf473fea98686382c6f2d54
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-11-20 22:20:34 +0200

    Add functions to display advertisements

---
+++ src/features/display-ads/handlers.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-param-reassign */
+const { telegramTemplate } = require('claudia-bot-builder');
+
+// ////////////////////////////////////////////////// //
+//                  Display data                      //
+// ////////////////////////////////////////////////// //
+
+exports.displayAllAds = (update) => {
+    update.userState.act = 'AD_DISPLAY_ALL_ACT'; // Todo Make constant
+    if (update.advertisements !== undefined) {
+        return update.advertisements.map(({ _id: id, title, author, description, renumeration }) => {
+            const callbackData = JSON.stringify({
+                idAd: id,
+                op: 'ADD_TO_SAVED'
+            });
+            return new telegramTemplate.Text(`
+            ===============================
+            [   Author](tg://user?id=${author})
+
+            *❗️ ${title} ❗️*
+
+            ${description}
+
+            🎁   ${renumeration}   🎁
+            ===============================
+            `)
+                .addInlineKeyboard([
+                    [
+                        {
+                            text: '❤️',
+                            callback_data: callbackData
+                        }
+                    ]
+                ])
+                .get();
+        });
+    }
+    // Todo - make constatns + lang
+    return 'Nothing to display';
+};
+
+exports.displayMyAds = (update) => {
+    update.userState.act = 'AD_DISPLAY_MY_ACT';
+    if (update.advertisements !== undefined) {
+        return update.advertisements.map(({ _id: id, title, author, description, renumeration }) => {
+            const callbackData = JSON.stringify({
+                idAd: id,
+                op: 'DELETE_MY_AD'
+            });
+            return new telegramTemplate.Text(`
+            ===============================
+            [   Author](tg://user?id=${author})
+
+            *❗️ ${title} ❗️*
+
+            ${description}
+
+            🎁   ${renumeration}   🎁
+            ===============================
+            `)
+                .addInlineKeyboard([
+                    [
+                        {
+                            text: '☠️',
+                            callback_data: callbackData
+                        }
+                    ]
+                ])
+                .get();
+        });
+    }
+    return 'Nothing to display';
+};
+
+exports.displaySavedAds = (update) => {
+    update.userState.act = 'AD_DISPLAY_SAVED_ACT';
+    if (update.advertisements !== undefined) {
+        return update.advertisements.map(({ _id: id, title, author, description, renumeration }) => {
+            const callbackData = JSON.stringify({
+                idAd: id,
+                op: 'DELETE_FROM_SAVED'
+            });
+            return new telegramTemplate.Text(`
+            ===============================
+            [   Author](tg://user?id=${author})
+
+            *❗️ ${title} ❗️*
+
+            ${description}
+
+            🎁   ${renumeration}   🎁
+            ===============================
+            `)
+                .addInlineKeyboard([
+                    [
+                        {
+                            text: '💀',
+                            callback_data: callbackData
+                        }
+                    ]
+                ])
+                .get();
+        });
+    }
+    return 'Nothing to display';
+};
