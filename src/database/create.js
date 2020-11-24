b3792f5ac43a5f41d30ff0f57b933c7b856f27d5
Alyona V <40433033+wild-arctic-fox@users.noreply.github.com>
2020-11-24 22:09:56 +0200

    Delete invalid comments

--- src/database/create.js
+++ src/database/create.js
@@ -1,14 +1,6 @@
 const { logger } = require('../helpers');
 const { UserModel, AdvertModel } = require('../models');
 
-/**
- * Create and save User object
- *
- * @async
- * @param {Number} _id - telegram ID
- * @param {string} lang
- * @param {string} appState
- */
 const createUser = async (user) => {
     try {
         const model = new UserModel(user);
@@ -19,12 +11,6 @@ const createUser = async (user) => {
     }
 };
 
-/**
- * Create and save Advertisement object
- *
- * @async
- * @property {Number} author - telegram ID
- */
 const createAdvertisement = async (advertisement) => {
     try {
         const model = new AdvertModel(advertisement);
