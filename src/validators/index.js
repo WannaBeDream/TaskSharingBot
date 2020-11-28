0a0e3dfbbe8b4c4f71e29b71dac6512ab222cee6
z347 <shulo.vova@gmail.com>
2020-11-29 01:44:37 +0200

    delete "envalid" npm module

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,6 +1,5 @@
 const { verificationDataForCompliance } = require('./is-advertisement');
 const userInputData = require('./user-input-data');
 const { noSpace } = require('./reg-exp');
-const { validatedConfig } = require('./config-validated');
 
-module.exports = { verificationDataForCompliance, noSpace, userInputData, validatedConfig };
+module.exports = { verificationDataForCompliance, noSpace, userInputData };
