368a97b5da6b2decc5a51c4b33dcab50406683b4
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-12-02 13:35:41 +0200

    Refactor template

--- src/features/ad-template.js
+++ src/features/ad-template.js
@@ -1,30 +1,34 @@
 const labels = require('./display-ads/labels');
 const { SPAM_COUNTER } = require('../constants/db-values');
 
+function createTemplateText(title, description, renumeration, author) {
+    return `\u26A1 *${title}*
+${renumeration}
+${description}
+${author}`;
+}
+
+function checkAd(ad, lang) {
+    // eslint-disable-next-line no-nested-ternary
+    return ad.spam.length >= SPAM_COUNTER
+        ? `*${labels.deletedSpam[`${lang}`]}*`
+        : ad.author
+        ? `[${labels.author[`${lang}`]}](tg://user?id=${ad.author})`
+        : `*${labels.deleted[`${lang}`]}*`;
+}
+
+function reduceText(ad, renumeration, author, length) {
+    const description = ad.description.slice(0, ad.description.length - (length - 1024) - 3).concat('...');
+    return createTemplateText(ad.title, description, renumeration, author);
+}
+
 exports.AD_TEMPLATE = (ad, lang, keyboard) => {
     const renumeration = ad.renumeration ? `💰 ${ad.renumeration}` : '';
-    const author =
-        // eslint-disable-next-line no-nested-ternary
-        ad.spam.length >= SPAM_COUNTER
-            ? `*${labels.deletedSpam[`${lang}`]}*`
-            : ad.author
-            ? `[${labels.author[`${lang}`]}](tg://user?id=${ad.author})`
-            : `*${labels.deleted[`${lang}`]}*`;
+    const author = checkAd(ad, lang);
     if (ad.imgId) {
-        let caption = `\u26A1 *${ad.title}*
-${renumeration}
-            
-${ad.description}
-            
-${author}`;
+        let caption = createTemplateText(ad.title, ad.description, renumeration, author);
         if (caption.length > 1024) {
-            const description = ad.description.slice(0, ad.description.length - (caption.length - 1024) - 3);
-            caption = `\u26A1 *${ad.title}*
-${renumeration}
-        
-${description}...
-        
-${author}`;
+            caption = reduceText(ad, renumeration, author, caption.length);
         }
         return {
             method: 'sendPhoto',
@@ -37,36 +41,16 @@ ${author}`;
         };
     }
 
-    return `\u26A1 *${ad.title}*
-${renumeration}
-${ad.description}
-${author}`;
+    return createTemplateText(ad.title, ad.description, renumeration, author);
 };
 
 exports.AD_TEMPLATE_TEXT = (ad, lang) => {
     const renumeration = ad.renumeration ? `💰 ${ad.renumeration}` : '';
-    const author =
-        // eslint-disable-next-line no-nested-ternary
-        ad.spam.length >= SPAM_COUNTER
-            ? `*${labels.deletedSpam[`${lang}`]}*`
-            : ad.author
-            ? `[${labels.author[`${lang}`]}](tg://user?id=${ad.author})`
-            : `*${labels.deleted[`${lang}`]}*`;
+    const author = checkAd(ad, lang);
 
-    let caption = `\u26A1 *${ad.title}*
-${renumeration}
-            
-${ad.description}
-            
-${author}`;
+    let caption = createTemplateText(ad.title, ad.description, renumeration, author);
     if (caption.length > 1024) {
-        const description = ad.description.slice(0, ad.description.length - (caption.length - 1024) - 3);
-        caption = `\u26A1 *${ad.title}*
-${renumeration}
-        
-${description}...
-        
-${author}`;
+        caption = reduceText(ad, renumeration, author, caption.length);
     }
     return caption;
 };
