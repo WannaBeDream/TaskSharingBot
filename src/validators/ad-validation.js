6a132058f1792f4117e91684fd304d7d29a3d6ec
z347 <shulo.vova@gmail.com>
2020-12-03 20:33:23 +0200

    delete logger from validators

--- src/validators/ad-validation.js
+++ src/validators/ad-validation.js
@@ -1,11 +1,9 @@
 const validator = require('validator');
-const { logger } = require('../helpers');
 
 const userInputData = {
     ifStrCondition: (str, value) => !validator.isLength(str, value.min, value.max),
     ifStrContain: (str, phrases) => {
         const res = phrases.includes(str);
-        logger.info(res);
         return !res;
     }
 };
