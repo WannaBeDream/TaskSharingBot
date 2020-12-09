de9ca35a0b18f5a82501484028b4de3a28e213b2
z347 <shulo.vova@gmail.com>
2020-12-09 17:20:13 +0200

    added button location at initNewUserSetLocationView

--- src/features/user-settings/handlers.js
+++ src/features/user-settings/handlers.js
@@ -12,7 +12,14 @@ const markdownUtils = require('../../helpers/markdown-utils');
 // ////////////////////////////////////////////////// //
 
 exports.initNewUserSetLocationView = (context) => {
-    return new Text(labels.newUserEnterLocation[context.lang]).replyKeyboardHide().get();
+    const message = labels.newUserEnterLocation[context.lang];
+    const setLocationButton = [{ text: labels.locationAutoSend[context.lang], request_location: true }];
+    const initCoordinates = [24.028013, 49.839781];
+
+    return [
+        new Text(message).replyKeyboardHide().get(),
+        new Location(initCoordinates[1], initCoordinates[0]).addReplyKeyboard([setLocationButton], true).get()
+    ];
 };
 
 exports.initNewUserSetRadiusView = (context) => {
